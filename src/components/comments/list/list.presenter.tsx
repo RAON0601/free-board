be3b6d5832872fa513c18657bb64cfe324c57821
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-02-04 09:56:35 +0900

    댓글 리스트 MUI로 이전

--- src/components/comments/list/list.presenter.tsx
+++ src/components/comments/list/list.presenter.tsx
@@ -1,22 +1,41 @@
-import { CommentContainer, CreatedAt, CommentItem, ItemRight, Writer, WriterBox, Content } from './list.style';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import PersonIcon from '@mui/icons-material/Person';
+import Stack from '@mui/material/Stack/Stack';
+import Typography from '@mui/material/Typography';
+
+import { formatYYYYMMDD } from '@/commons/utils';
 import type { CommentListUIProps } from './list.types';
 
 export default function CommentListUI({ commentList }: CommentListUIProps) {
   return (
-    <CommentContainer>
+    <List sx={{ width: '50vw', minWidth: '768px', margin: '20px auto 0 auto' }}>
       {commentList?.map(comment => (
-        <CommentItem key={comment._id}>
-          <span>프로필이미지</span>
-          <ItemRight>
-            <WriterBox>
-              <Writer>{comment.writer}</Writer>
-              <span style={{ marginLeft: '8px' }}>{comment.rating}</span>
-            </WriterBox>
-            <Content>{comment.contents}</Content>
-            <CreatedAt>{comment.createdAt}</CreatedAt>
-          </ItemRight>
-        </CommentItem>
+        <ListItem key={comment._id} sx={{ borderBottom: '1px solid #bdbdbd' }}>
+          <Stack flexDirection="column" alignItems="center" justifyContent="center" sx={{ marginRight: '16px' }}>
+            <ListItemAvatar>
+              <Avatar sx={{ margin: '0 auto' }}>
+                <PersonIcon fontSize="large" />
+              </Avatar>
+            </ListItemAvatar>
+            <Typography variant="subtitle1" align="center">
+              {comment.writer}
+            </Typography>
+          </Stack>
+
+          <ListItemText
+            primary={
+              <Typography variant="body1" sx={{ marginBottom: '12px' }}>
+                {comment.contents}
+              </Typography>
+            }
+            secondary={<Typography variant="caption">{formatYYYYMMDD(comment.createdAt)}</Typography>}
+          />
+        </ListItem>
       ))}
-    </CommentContainer>
+    </List>
   );
 }
