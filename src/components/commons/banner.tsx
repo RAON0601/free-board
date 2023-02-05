703700a97283c1c48fa6795d43d810e752d64450
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-02-05 21:46:55 +0900

    메인 페이지 케러셀 추가

--- src/components/commons/banner.tsx
+++ src/components/commons/banner.tsx
@@ -1,3 +1,29 @@
-export default function Banner() {
-  return <div>배너</div>;
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export function Banner() {
+  return (
+    <>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={swiper => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide style={{ height: '400px' }}>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 import { Layout } from '@/components/commons/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
